adfdc3e81dbfcbb8c2e4c91aa3a341d589b5e4d3
DannyCos <DannyCos@users.noreply.github.com>
2024-07-31 21:27:57 +0100

    color changes

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 30;        /* gaps between windows */
 static const unsigned int snap      = 30;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -18,9 +18,9 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg        	 bg         	border   */
+	[SchemeNorm] = { "#b4befe",	"#1E1E2E", 	"#1e1e2e" },
+	[SchemeSel]  = { "#1e1e2e",	"#eba0ac", 	"#89dceb"  },
 };
 
 /* tagging */
