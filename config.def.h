f2d48b71274ef5eee3c9882b83ca5d87a8b51000
DannyCos <DannyCos@users.noreply.github.com>
2024-07-31 13:59:47 +0100

    Added alpha patch

--- config.def.h
+++ config.def.h
@@ -9,6 +9,8 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
 static const char *fonts[]          = { "Iosevka:size=12" };
 static const char dmenufont[]       = "Hack:size=12";
+static unsigned int baralpha        = 0xd0;
+static unsigned int borderalpha     = OPAQUE;
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
