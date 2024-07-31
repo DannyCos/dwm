5fb7d58c2bc67c12a8b4b663f8d2aa709b63d08d
DannyCos <DannyCos@users.noreply.github.com>
2024-07-31 13:59:01 +0100

    Gaps config

--- config.def.h
+++ config.def.h
@@ -2,8 +2,8 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx     = 30;        /* gaps between windows */
+static const unsigned int snap      = 30;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
